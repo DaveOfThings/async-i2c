f9a08d08b622661f14e4b602fab09ada62551ed7
David Wheeler <dwheeler20874@gmail.com>
2025-07-25 23:53:59 -0400

    Reading IMU data works.
    Lots of hard-coded constants should be cleaned up.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -38,16 +38,57 @@ async fn i2c_task(i2c_periph: AnyI2c<'static>, sda: AnyPin<'static>, scl: AnyPin
         .with_scl(scl)
         .into_async();
 
+    // TODO-DW : Configure IMU and read it
+    // Set 6-Axis low noise mode (turn on acccel and gyro) 0x1F PWR_MGMT
+    // Configure Accel 0, Accel 1
+    // Configure Gyro 0, Gyro 1
+    // FIFO Config?
+    // Int Source?
+
+    let config_buf = [0x1F, // Start writing to PWR_MGMT0 reg
+        0x0F,   // PWR_MGMT0: RC Osc off, Gyro and Accel in Low Noise Mode
+        0x69,   // GYRO_CONFIG0: +/-250 dps, Gyro ODR: 100Hz 
+        0x69,   // ACCEL_CONFIG0: +/- 2g, Accel ODR: 100Hz
+        0x60,   // TEMP_CONFIG0: LPF: 4 Hz
+        0x05,   // GYRO_CONFIG1: LPF: 34 Hz
+        0x05,   // ACCEL_CONFIG1: 2x averaging for LPM, 34 Hz LPF
+        ];
+    let _ = i2c0.write_async(0x68, &config_buf).await;
+    println!("Configured IMU.");
+
     let wr_buf = [0x75];    // Who am I request
     let mut rd_buf = [0x00; 1]; // Who Am I response
 
     let _result = i2c0.write_read_async(0x68, &wr_buf, &mut rd_buf).await;
 
-    println!("Read Who am I: 0x{:02x}", rd_buf[0]);
+    println!("Actually read Who am I: 0x{:02x}", rd_buf[0]);
 
-    // TODO-DW : Configure IMU and read it
+    let mut count = 0;
     loop {
-        Timer::after(Duration::from_secs(1)).await;
+        Timer::after(Duration::from_millis(5)).await;
+
+        let ready_query = [0x39];
+        let mut ready: [u8; 1] = [0];
+
+
+        // Check for new data (read 0x39, use DATA_RDY_INT bit)
+        let _ = i2c0.write_read_async(0x68, &ready_query, &mut ready).await; 
+        if (ready[0] & 1) != 0 {
+            count += 1;
+            // println!("{} Data ready.", count);
+
+            let data_query = [0x09];
+            let mut data_response = [0; 14];
+
+            let _ = i2c0.write_read_async(0x68, &data_query, &mut data_response).await;
+            println!("AX = {:02X}{:02X}", data_response[2], data_response[3]);
+        }
+        else {
+            // println!("---");
+        }
+
+
+        // Read Raw Accel, Raw Gyro.
     }
 }
 
